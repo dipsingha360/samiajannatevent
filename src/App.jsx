9c5a244d6f98582929ca8cc6b05a6c997d98925c
Dip Singha <dipsingha2@gmail.com>
2023-07-13 12:55:44 +0600

    react routere added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Services from "./pages/Services";
+import Team from "./pages/Team";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+      <div className="h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="samiajannatevent/" element={<Home />} />
+          <Route path="samiajannatevent/about" element={<About />} />
+          <Route path="samiajannatevent/services" element={<Services />} />
+          <Route path="samiajannatevent/team" element={<Team />} />
+          <Route path="samiajannatevent/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
       <Footer />
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 function Navbar() {
   return (
     <div className="  py-4 px-40 flex items-center justify-between border-b border-yellow-50/10 ">
@@ -6,19 +8,17 @@ function Navbar() {
       </div>
       <ul className="flex items-center gap-8">
         <li className="cursor-pointer text-yellow-50 hover:text-yellow-500 duration-300">
-          Home
+          <NavLink to="samiajannatevent/">Home</NavLink>
         </li>
         <li className="cursor-pointer text-yellow-50 hover:text-yellow-500 duration-300">
-          About
+          <NavLink to="samiajannatevent/about">About</NavLink>
+        </li>
+        <li className="cursor-pointer text-yellow-50 hover:text-yellow-500 duration-300"></li>
+        <li className="cursor-pointer text-yellow-50 hover:text-yellow-500 duration-300">
+          <NavLink to="samiajannatevent/team">Team</NavLink>
         </li>
         <li className="cursor-pointer text-yellow-50 hover:text-yellow-500 duration-300">
-          Services
-        </li>
-        <li className="cursor-pointer text-yellow-50 hover:text-yellow-500 duration-300">
-          Team
-        </li>
-        <li className="cursor-pointer text-yellow-50 hover:text-yellow-500 duration-300">
-          Contact
+          <NavLink to="samiajannatevent/contact">Contact</NavLink>
         </li>
       </ul>
     </div>
